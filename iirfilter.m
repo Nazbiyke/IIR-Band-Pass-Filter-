02de302d6bde192e3f218da6d34b1fe201668de8
Nazbiyke <100347662+Nazbiyke@users.noreply.github.com>
2025-12-06 16:06:57 +0100

    Update and rename exam.m to iirfilter.m

--- iirfilter.m
+++ iirfilter.m
@@ -1,12 +1,11 @@
- % IIR Bandpass Filter Design with Phase Equalization
+% IIR Bandpass Filter Design with Phase Equalization
 clf;
 clc;
 clear;
 
-
 % Part 1: Design IIR Bandpass Butterworth Filter
 
-% Filter specifications
+% Filter specifications:
 %maximum attenuation in Passband  = 1.5 dB;           
 %minimum attenuation in SB = 31 dB
 Fp1 = 4000;      % Lower passband edge (4 kHz)
@@ -21,11 +20,12 @@ ws = [Fs1, Fs2]/(Sf/2);
 
 % Design Butterworth bandpass filter
 [N, Wn] = buttord(wp, ws, 1.5, 31);
-[b1, a1] = butter(N, Wn, 'z');
+[b1, a1] = butter(N, Wn, 'bandpass');  
 
 % Frequency response
 w = pi*(0:0.001:1); % Normalized frequency vector
 h1 = freqz(b1, a1, w);
+g1 = grpdelay(b1, a1, w); % group delay of the bandpass filter
 
 % Plot poles and zeros
 figure(1);
@@ -38,29 +38,27 @@ plot(w/pi, 20*log10(abs(h1))); grid on;
 xlabel('\omega /\pi'); ylabel('Gain (dB)');
 title('Bandpass Filter Frequency Response');
 
-pause;
-
 
 % Part 2: Group Delay Equalization with All-Pass
 
-
 % Design all-pass filter for equalization
-N = 4 ; % Use same order as bandpass filter
-F = w(1:501)/pi; % Normalized frequencies
+N_ap = 4; % All-pass filter order
+indx = round(wp / 0.001) + 1; % Indices of the pass-band frequencies 
+F = w(indx(1): indx(2))/pi; % Normalized frequencies
 edges = [F(1) F(end)]; % Band edges
 
-% Desired group delay desired group-delays of APF (>0) 
-Gd = max(g1)-g1(1:501); 
+% Desired group delay
+Gd = max(g1)-g1(indx(1):indx(2)); 
 
 % Design all-pass filter
-[bap, aap] = iirgrpdelay(N, F, edges, Gd);
+[b2, a2] = iirgrpdelay(N_ap, F, edges, Gd);
 
 % All-pass frequency response calculation
-hap = freqz(b2, a2, w);
-gap = grpdelay(b2, a2, w);
+h2 = freqz(b2, a2, w);
+g2 = grpdelay(b2, a2, w);
 
 % Plot of APF frequency response magnitude, phase and group delay
-figure(4)
+figure(3)
 subplot(3,1,1)
 plot(w/pi, abs(h2)); grid on;
 ylabel('magnitude response');
@@ -75,7 +73,6 @@ plot(w/pi, g2); grid on;
 ylabel('group delay (samples)');
 xlabel('Normalized Frequency (\times\pi rad/sample)');
 
-pause;
 
 % Cascade of the two filters
 b = conv(b1, b2); %product of numerator polynomials
@@ -84,17 +81,16 @@ h = freqz(b, a, w); % compute DTFT
 g = grpdelay(b, a, w); % compute group delay
 
 % Plot of resulting filter 
-figure(5)
-subplot(3,1,1)
+figure(4)
+subplot(4,1,1)
 plot(w/pi, abs(h)); grid on;
 ylabel('magnitude response');
 title('Cascaded System: Bandpass + All-Pass');
-
-subplot(3,1,2)
+subplot(4,1,2)
 plot(w/pi, unwrap(angle(h))); grid on;
 ylabel('phase response (rad)');
-
-subplot(3,1,3)
+subplot(4,1,3)
 plot(w/pi, g); grid on;
 ylabel('group delay (samples)');
 xlabel('Normalized Frequency (\times\pi rad/sample)');
+
